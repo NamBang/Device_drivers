e976fc435e81b511eb7b0d523c12e2bc5f24f34e
nambang <nambangn@gmail.com>
2019-04-05 22:12:49 +0700

    test2

--- test/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-make clean
-make mrproper
-make ARCH=arm CROSS_COMPILE=/home/nambang/Documents/DriverLinux/Document/source_code/gcc-linaro-arm/gcc-linaro/bin/arm-linux-gnueabi- sun8iw7p1smp_defconfig
-make 
-
-
