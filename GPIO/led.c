5a39d8b848ee251fa8922b7a573d458daba6745e
nambang <nambangn@gmail.com>
2019-07-24 21:26:03 +0700

    GPIO LED V1.1

--- GPIO/led.c
+++ GPIO/led.c
@@ -17,16 +17,16 @@
 #define LED_OFF 0
 
 #define BASE_ADDR 0x01C20800
-//PA7
+/* PA7 PA9*/
 #define LED_GREEN 7
 #define LED_RED 9
-#define OFF_SET_CONFIG_REG 0x00
+#define OFF_SET_COFIG_REG_PA_0_7 0x00
 
-#define OFF_SET_CONFIG 28
+#define OFF_SET_CONFIG_PA7 28
 #define DATA_REG 0x10
-
+/*Button PA8 */
 #define BUTTON 8
-#define OFFSET_GPIOA 0x04
+#define OFF_SET_COFIG_REG_PA_8_15 0x04
 #define OFFSET_GPIOA_PULL 0x1C
 #define OFFSET_EXTERNAL_INTERRUPT_GPIOA 0x200
 #define SIZE_EXTERNAL_INTERRUPT_GPIOA 0x20
@@ -36,10 +36,10 @@
 /*variable */
 static struct timer_list exp_timer;
 unsigned char irq_eint0;
-unsigned int *gpioa_0_7;
-unsigned int *ex_gpioa_0_7;
+unsigned int *GPIO_8_15;
+unsigned int *ex_GPIO_8_15;
 unsigned int *gpioa_pull_0_15;
-/* prototypes */
+/* entry point */
 int leds_open(struct inode *inode, struct file *file);
 ssize_t leds_read(struct file *file, char *buf, size_t count, loff_t *ppos);
 ssize_t leds_write(struct file *file, const char *buf, size_t count, loff_t *ppos);
@@ -50,10 +50,11 @@ struct _vchar_drv{
 	dev_t dev_num;
 	struct class *dev_class;
 	struct device *dev;
-	/*cap phat bo nho*/
+	/*Memory allocation*/
 	struct cdev *vcdev;
 	unsigned int open_cnt;
 }vchar_drv;
+
 /* file operations structure */
 static struct file_operations leds_fops = {
     .owner = THIS_MODULE,
@@ -71,22 +72,20 @@ static unsigned long temp;
 void initLedPort(void)
 {
     io = ioremap(BASE_ADDR, 0x100);
-    //pr_info("IO %x\n", io);
     if (io == NULL)
     {
         pr_alert("fail map base address\n");
         //return -1;
     }
-    temp = ioread32(io + OFF_SET_CONFIG_REG);
-    //pr_info("before config reg %x\n", temp);
+    temp = ioread32(io + OFF_SET_COFIG_REG_PA_0_7);
 
     // set config mode output for pa7
-    temp = temp & (~(0x7 << OFF_SET_CONFIG));
+    temp = temp & (~(0x7 << OFF_SET_CONFIG_PA7));
 
     // set mode output
-    temp |= (0x01 << OFF_SET_CONFIG);
+    temp |= (0x01 << OFF_SET_CONFIG_PA7);
 
-    iowrite32(temp, (io + OFF_SET_CONFIG_REG));
+    iowrite32(temp, (io + OFF_SET_COFIG_REG_PA_0_7));
 	temp = ioread32(io + DATA_REG);
 	temp |= (1 << LED_GREEN);
 	iowrite32(temp, (io + DATA_REG));
@@ -126,7 +125,7 @@ void toggle(int led_number)
 /*interrupt handler */
 irqreturn_t external_interrupt_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
-	printk("before [0]=%x ---[4]=%x/n",&(gpioa_0_7[0]),&(gpioa_0_7[4])); 
+	printk("before [0]=%x ---[4]=%x/n",&(GPIO_8_15[0]),&(GPIO_8_15[4])); 
     toggle(LED_GREEN);
 	return IRQ_HANDLED;
 
@@ -137,24 +136,6 @@ static void timer_led(unsigned long);
 static void timer_led(unsigned long data)
 {
 	unsigned int delay = 1 * HZ;
-   // printk("in timer\n");
-/*	if(((address_gpioa[4] & (1 << 0)) == 0) && flag == 1)
-	{
-		flag = 0;
-		if((address_gpioa[4] & (1 << 1)) == 0)
-		{
-			address_gpioa[4] |= (1 << 1);
-			printk("LED ON\n");
-		}
-		else
-		{
-			address_gpioa[4] &= ~(1 << 1);
-			printk("LED OFF\n");
-		}
-	}
-	else if(((address_gpioa[4] & (1 << 0)) == 1) && flag == 0)
-		flag = 1;
-	*/
     toggle(LED_RED);
 	exp_timer.expires = jiffies + delay;
 	exp_timer.function = (void *)timer_led;
@@ -193,7 +174,7 @@ int __init leds_init(void)
  		goto failed_create_device;
  	}
 
-	/*dang ky entry point voi kernel*/
+	/*register entry point to kernel*/
 	vchar_drv.vcdev = cdev_alloc();
 	if(vchar_drv.vcdev == NULL)
 	{
@@ -207,33 +188,29 @@ int __init leds_init(void)
 	}
 
     /*interrupt */
-    gpioa_0_7 = ioremap(BASE_ADDR + OFFSET_GPIOA, SIZE_GPIOA);
-	if(gpioa_0_7 == NULL)
+    GPIO_8_15 = ioremap(BASE_ADDR + OFF_SET_COFIG_REG_PA_8_15, SIZE_GPIOA);
+	if(GPIO_8_15 == NULL)
 	{
 		printk("Mapping address gpioa fail\n");
 		return -1;
 	}
 
-	//Select EINT0 mode GPIOA_0
-	gpioa_0_7[0] &= ~(7 << 0);
-	gpioa_0_7[0] |= (6 << 0);
+	//Select EINT0 mode GPIOA_8
+	GPIO_8_15[0] &= ~(7 << 0);
+	GPIO_8_15[0] |= (6 << 0);
 
-	//Select output mode GPIOA_1
-   
-	gpioa_0_7[0] &= ~(7 << 4);
-	gpioa_0_7[0] |= (1 << 4);
     
 	//Mapping EINT_GPIOA
-	ex_gpioa_0_7 = ioremap(BASE_ADDR + OFFSET_EXTERNAL_INTERRUPT_GPIOA, SIZE_EXTERNAL_INTERRUPT_GPIOA);
-	if(ex_gpioa_0_7 == NULL)
+	ex_GPIO_8_15 = ioremap(BASE_ADDR + OFFSET_EXTERNAL_INTERRUPT_GPIOA, SIZE_EXTERNAL_INTERRUPT_GPIOA);
+	if(ex_GPIO_8_15 == NULL)
 	{
 		printk("Mapping ex_gpioa fail\n");
 		return -1;
 	}
 
 	//EINT0 Positive Edge
-	ex_gpioa_0_7[0] &= ~(15 << 0);
-	ex_gpioa_0_7[0] |= (1 << 0);
+	ex_GPIO_8_15[0] &= ~(15 << 0);
+	ex_GPIO_8_15[0] |= (1 << 0);
 
 	//Mapping gpioa pull
 	gpioa_pull_0_15 = ioremap(BASE_ADDR + OFFSET_GPIOA_PULL, SIZE_GPIOA_PULL);
@@ -242,7 +219,7 @@ int __init leds_init(void)
 		printk("Mapping gpioa pull fail!\n");
 		return -1;
 	}
-	//GPIOA0 PULL UP
+	//GPIOA8 PULL UP
 	gpioa_pull_0_15[0] &= ~(3 << 0);
 	gpioa_pull_0_15[0] |= (1 << 0);
 
@@ -253,12 +230,15 @@ int __init leds_init(void)
 	if(ret)
 		printk("Init EINT0 success\n");
     /*end interrupt */
-    /*timer */
+    /*init timer */
     exp_timer.expires = jiffies + 1 * HZ;
 	exp_timer.function = (void *)timer_led;
-	//exp_timer.data = NULL;
 
-	//Bat dau timer
+	//Select output mode GPIOA_9
+   
+	GPIO_8_15[0] &= ~(7 << 4);
+	GPIO_8_15[0] |= (1 << 4);
+	/*start timer */ 
 	add_timer(&exp_timer);
     return 0;
 
@@ -278,16 +258,16 @@ void __exit leds_exit(void)
 	temp = ioread32(io + DATA_REG);
 	temp &= ~( 1 << LED_GREEN);
 	iowrite32(temp , (io +DATA_REG));
-	/*huy dang ky entry point*/
+	/*destroy entry point*/
 	cdev_del(vchar_drv.vcdev);
-	/* xoa bo device file*/
+	/* remove device file*/
 	device_destroy(vchar_drv.dev_class,vchar_drv.dev_num);
 	class_destroy(vchar_drv.dev_class);
-	/* giai phong device number*/
+	/* free device number*/
 	unregister_chrdev_region(vchar_drv.dev_num,1);
 	printk("exit vchar driver ");
-    iounmap(gpioa_0_7);
-	iounmap(ex_gpioa_0_7);
+    iounmap(GPIO_8_15);
+	iounmap(ex_GPIO_8_15);
 	iounmap(gpioa_pull_0_15);
 	free_irq(irq_eint0, (void *)external_interrupt_handler);
 }
@@ -324,7 +304,7 @@ ssize_t leds_read(struct file *file, char *buf, size_t count, loff_t *ppos)
     *ppos += num_bytes;
     return num_bytes;
 }
-
+/*write led */
 ssize_t leds_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
 {
 
@@ -357,5 +337,5 @@ ssize_t leds_write(struct file *file, const char *buf, size_t count, loff_t *ppo
 
 module_init(leds_init);
 module_exit(leds_exit);
-MODULE_AUTHOR("hethongnhung.com");
+MODULE_AUTHOR("Nhom_AtoZ");
 MODULE_LICENSE("GPL");
